8073584a240438cadf08e60bf91208fc8392e6c6
Agus Setiawan <kangcinho@gmail.com>
2020-10-05 16:42:58 +0800

    Add CRUD to Category Table

--- app/Model/Category.php
+++ app/Model/Category.php
@@ -6,11 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $fillable = ['name', 'slug', 'user_id'];
+
     public function question(){
         return $this->hasMany('App\Model\Question', 'category_id', 'id');
     }
 
     public function user(){
         return $this->belongsTo('App\Model\User', 'user_id');
+    }
+
+    public function getRouteKeyName(){
+        return 'slug';
     }
 }
